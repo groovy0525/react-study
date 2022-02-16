b6ab39cb8e63dd6169ef86fda9a5858d42cc9711
groovy0525 <60678333+groovy0525@users.noreply.github.com>
2022-02-17 05:27:50 +0900

    fix: parameter - done

--- week_3/test/callback.test.js
+++ week_3/test/callback.test.js
@@ -13,12 +13,12 @@ describe("Callback - fs.readFile를 콜백 패턴만 사용", () => {
     fs.readFile("assets/first.json", (err, data) => {
       const first = JSON.parse(data);
       const key = first.second_key;
-      done();
 
       fs.readFile("assets/second.json", (err, data) => {
         const second = JSON.parse(data);
         const result = second.find(item => item.key === key);
         expect(result.hi).toBe("Second 방가방가");
+        done();
       });
     });
   });
@@ -27,17 +27,16 @@ describe("Callback - fs.readFile를 콜백 패턴만 사용", () => {
     fs.readFile("assets/first.json", (err, data) => {
       const first = JSON.parse(data);
       const key = first.second_key;
-      done();
 
       fs.readFile("assets/second.json", (err, data) => {
         const second = JSON.parse(data);
         const thirdKey = second.find(item => item.key === key).third_key;
-        done();
 
         fs.readFile("assets/third.json", (err, data) => {
           const third = JSON.parse(data);
           const result = third.find(item => item.key === thirdKey);
           expect(result.hi).toBe("Third 방가방가");
+          done();
         });
       });
     });
--- week_3/test/promise.test.js
+++ week_3/test/promise.test.js
@@ -8,16 +8,15 @@ describe("Promise - fs.readFile를 프로미스 패턴만 사용", () => {
     expect(promise instanceof Promise).toBe(true);
   });
 
-  test("assets/first.json을 불러오기", () => {
-    readFile("assets/first.json")
-      .then(data => {
-        const first = JSON.parse(data);
-        expect(first.hi).toBe("방가방가");
-      })
-      .catch(err => err);
+  test("assets/first.json을 불러오기", done => {
+    readFile("assets/first.json").then(data => {
+      const first = JSON.parse(data);
+      expect(first.hi).toBe("방가방가");
+      done();
+    });
   });
 
-  test("assets/first.json에서 불러온 second_key를 이용해 second.json에서 key에 일치하는 오브젝트 찾기", () => {
+  test("assets/first.json에서 불러온 second_key를 이용해 second.json에서 key에 일치하는 오브젝트 찾기", done => {
     readFile("assets/first.json")
       .then(data => {
         const first = JSON.parse(data);
@@ -28,12 +27,12 @@ describe("Promise - fs.readFile를 프로미스 패턴만 사용", () => {
           const second = JSON.parse(data);
           const result = second.find(item => item.key === key);
           expect(result.hi).toBe("Second 방가방가");
+          done();
         });
-      })
-      .catch(err => err);
+      });
   });
 
-  test("assets/second.json에서 불러온 third_key를 이용해 third.json에서 key에 일치하는 오브젝트 찾기", () => {
+  test("assets/second.json에서 불러온 third_key를 이용해 third.json에서 key에 일치하는 오브젝트 찾기", done => {
     readFile("assets/first.json")
       .then(data => {
         const first = JSON.parse(data);
@@ -51,8 +50,8 @@ describe("Promise - fs.readFile를 프로미스 패턴만 사용", () => {
           const third = JSON.parse(data);
           const result = third.find(item => item.key === thirdKey);
           expect(result.hi).toBe("Third 방가방가");
+          done();
         });
-      })
-      .catch(err => err);
+      });
   });
 });
